5aa418692a36399c8b397d29ca13029dae373939
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-01-24 23:03:20 -0800

    exercise#2

--- code.js
+++ code.js
@@ -2,20 +2,6 @@
 function helloWorld() {
     return "Hello, World!";
 }
-describe('sayHello', function() {
-    it('should be a defined function', function() {
-        expect(typeof sayHello).toBe('function');
-    });
-    it('should return a string when called', function() {
-        expect(typeof sayHello()).toBe("string");
-    });
-    it('should return the string "Hello, World!" when executed', function() {
-        expect(sayHello()).toBe("Hello, World!");
-    });
-    it("should never return 'undefined' when called", function() {
-        expect(sayHello()).not.toBe(undefined);
-    });
-});
 function sayHello() {
-
+    return '';
 }
